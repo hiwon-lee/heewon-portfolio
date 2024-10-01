6121557ea0cc4c291667fde2f7f78087c9b05ad8
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-10-02 02:00:49 +0900

    docs : 이미지 한방에 연결 ㄱㄱ~

---
+++ my-app/src/img/images.jsx
@@ -0,0 +1,52 @@
+// src/img/index.js
+import BLOOBURN1 from './blooburn01.png';
+import BLOOBURN2 from './blooburn02.png';
+import BLOOBURN3 from './blooburn03.png';
+import BLOOBURN4 from './blooburn04.png';
+import BLOOBURN5 from './blooburn05.png';
+import OWERE1 from './owere01.png';
+import OWERE2 from './owere02.png';
+import OWERE3 from './owere03.png';
+import OWERE4 from './owere04.png';
+import OWERE5 from './owere05.png';
+import OWERE6 from './owere06.png';
+import OWERE7 from './owere07.png';
+import MOTTNANI1 from './mottnani01.png';
+import MOTTNANI2 from './mottnani02.png';
+import MOTTNANI3 from './mottnani03.png';
+import MOTTNANI4 from './mottnani04.png';
+import MOTTNANI5 from './mottnani05.png';
+import HWPF1 from './hwpf01.png';
+import HWPF2 from './hwpf02.png';
+import HWPF3 from './hwpf03.png';
+import HWPF4 from './hwpf04.png';
+import ECST1 from './ECST01.png';
+import ECST2 from './ECST02.png';
+import ECST3 from './ECST03.png';
+
+export {
+  BLOOBURN1,
+  BLOOBURN2,
+  BLOOBURN3,
+  BLOOBURN4,
+  BLOOBURN5,
+  OWERE1,
+  OWERE2,
+  OWERE3,
+  OWERE4,
+  OWERE5,
+  OWERE6,
+  OWERE7,
+  MOTTNANI1,
+  MOTTNANI2,
+  MOTTNANI3,
+  MOTTNANI4,
+  MOTTNANI5,
+  HWPF1,
+  HWPF2,
+  HWPF3,
+  HWPF4,
+  ECST1,
+  ECST2,
+  ECST3,
+};
